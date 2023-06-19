79ff74b2aaf9bf66a814fa17a101e417f9ce751b
Chinika Charles <clcharles@hotmail.co.uk>
2023-06-19 19:35:32 +0100

    Added a selectedCategory variable and styling to the category buttons

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,6 +1,8 @@
 import { Stack } from '@mui/material';
 import { categories } from '../utils/constants';
 
+const selectedCategory = 'New';
+
 const Sidebar = () => (
     <Stack
     direct="row"
@@ -10,7 +12,9 @@ const Sidebar = () => (
     }}
     >
       {categories.map((category) => (
-        <button className="category-btn">
+        <button className="category-btn"
+        style={{ background: category.name === selectedCategory && '#FC1503',
+        color: 'white'}}>
           <span>{category.icon}</span>
           <span>{category.name}</span>
         </button>
