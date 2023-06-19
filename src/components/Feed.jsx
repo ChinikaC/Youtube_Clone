ccf300b363e7e60eb4298958d139b2d9d0d6b3aa
Chinika Charles <clcharles@hotmail.co.uk>
2023-06-19 01:13:03 +0100

    Added in stack, box and typography to the feed component

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,8 +1,27 @@
-import React from 'react'
+import { useState, useEffect} from 'react'
+import { Box, Stack, Typography} from '@mui/material';
 
 const Feed = () => {
   return (
-    <div>Feed</div>
+    <Stack sx={{ 
+      flexDirection: { sx: 
+    "coloumn", md: "row"}}}> // sx means usually, so usually the content on the page
+    // will be in a form of a column (on a mobile device) but on medium sized devices 
+    and larger (laptops, desktops or tablets), it will be in the form of a row
+    
+    <Box sx={{ height: { sx: 'auto', md: '92vh'}, 
+    borderLeft: '1px solid #3d3d3d', 
+  px: { sx: 0, md: 4}}}>
+    Sidebar
+
+    <Typography className="copyright"
+    variant="body2" sx={{mt: 1.5, color: '#fff'}}>
+      Copyright 2023 CC Media
+    </Typography>
+
+    </Box>
+
+    </Stack>
   )
 }
 
