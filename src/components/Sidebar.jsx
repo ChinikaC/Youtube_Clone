177cd8bab8b37742c282a6310e747a7afaf4e738
Chinika Charles <clcharles@hotmail.co.uk>
2023-06-19 19:26:31 +0100

    Added in styling and a button for the different ccategories for the sidebar

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,22 @@
+import { Stack } from '@mui/material';
+import { categories } from '../utils/constants';
+
+const Sidebar = () => (
+    <Stack
+    direct="row"
+    sx={{ overflowY: "auto",
+    height: { sx: 'auto', md: '95%' },
+    flexDirection: { md: 'column'}
+    }}
+    >
+      {categories.map((category) => (
+        <button className="category-btn">
+          <span>{category.icon}</span>
+          <span>{category.name}</span>
+        </button>
+      ))}
+    </Stack>
+  )
+
+
+export default Sidebar
