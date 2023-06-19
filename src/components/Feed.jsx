37e9ef5ba9a40ae8b8a6dc2735f717b50ae0bdae
Chinika Charles <clcharles@hotmail.co.uk>
2023-06-19 01:18:36 +0100

    Fixed issue with side bar

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -5,13 +5,14 @@ const Feed = () => {
   return (
     <Stack sx={{ 
       flexDirection: { sx: 
-    "coloumn", md: "row"}}}> // sx means usually, so usually the content on the page
-    // will be in a form of a column (on a mobile device) but on medium sized devices 
+      "coloumn", md: "row"}}}>
+    {/* sx means usually, so usually the content on the page
+    will be in a form of a column (on a mobile device) but on medium sized devices 
     and larger (laptops, desktops or tablets), it will be in the form of a row
-    
+     */}
     <Box sx={{ height: { sx: 'auto', md: '92vh'}, 
-    borderLeft: '1px solid #3d3d3d', 
-  px: { sx: 0, md: 4}}}>
+    borderRight: '1px solid #3d3d3d', 
+  px: { sx: 0, md: 2}}}>
     Sidebar
 
     <Typography className="copyright"
