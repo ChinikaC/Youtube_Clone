98a375a9cc85f786e83d56e9026481ef6736cb31
Chinika Charles <clcharles@hotmail.co.uk>
2023-06-30 20:15:58 +0100

    Added in a stack and box component for the Videos section

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -6,6 +6,8 @@ const Videos = ({videos}) => {
     <Stack direction="row" flex="wrap" justifyContent="start" gap={2}>
       {videos.map((item, idx) =>(
         <Box key={idx}>
+          {item.id.videoId && <VideoCard video={item}/>}
+          {item.id.channelId && <ChannelCard channelDetail={item}/>}
 
         </Box>
 
