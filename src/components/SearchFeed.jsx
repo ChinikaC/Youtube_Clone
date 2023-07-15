c91dfcd380d31240f322e016a290a510df62dfa3
Chinika Charles <clcharles@hotmail.co.uk>
2023-07-15 01:22:52 +0100

    Added code to the SearchFeed component

--- src/components/SearchFeed.jsx
+++ src/components/SearchFeed.jsx
@@ -1,8 +1,28 @@
-import React from 'react'
+import { useState, useEffect } from 'react'
+import { Box, Typography } from '@mui/material';
+import { fetchFromAPI } from '../utils/fetchFromAPI';
+import Videos from './Videos';
+import { useParams } from 'react-router-dom';
 
 const SearchFeed = () => {
+  const [videos, setVideos] = useState([]);
+  const { searchTerm } = useParams();
+  useEffect(() => {
+    fetchFromAPI(`search?part=snippet&q={searchTerm}`)
+    .then((data) => setVideos (data.items))
+  }, [searchTerm]);
+  {/* q = query */ }
+
   return (
-    <div>SearchFeed</div>
+     <Box p={2} sx={{ overFlowY: 'auto', height: '90vh', flex: 2 }}>
+        <Typography variant="h4" fontWeight="bold" mb={2} sx={{ color: 'white' }}>
+          Search Results for: <span style={{ color: '#F31503' }}>videos {searchTerm}</span> videos
+        </Typography>
+
+        <Videos videos={videos}/>
+
+      </Box>
+    
   )
 }
 
