c2ed5f2532a7f869c4b426effc71279d8bdc02cf
Chinika Charles <clcharles@hotmail.co.uk>
2023-07-20 00:32:59 +0100

    Formatted document

--- src/components/VideoDetail.jsx
+++ src/components/VideoDetail.jsx
@@ -50,11 +50,8 @@ const VideoDetail = () => {
                   {parseInt(likeCount).toLocaleString()} likes
                 </Typography>
               </Stack>
-
             </Stack>
-
           </Box>
-
         </Box>
         <Box px={2} py={{md: 1, xs: 5}} justifyContent="center" alignItems="center">
         <Videos videos={videos} direction="column"/>
